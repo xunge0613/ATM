488683e0c3e6c3ddb8c5bf9b25d91920dea86c7b
unknown <xxdalston@gmail.com>
2017-04-07 03:29:37 +0800

    update v0.0.4 rewrite rule matching, redefine api args,

--- demo/index.js
+++ demo/index.js
@@ -1,21 +1,25 @@
 // auto collect example 1
-ATM.autoCollectTrackData({
-	test3: 1 //  validate not passed
-},{
-	trigger: 'click',
-	page: '*',
-	element: '#test',
-	validateRule: 'test'
-})
+let trigger = 'click' // listen click event
+let page = '.*' // all pages
+let element2 = '.js-test-class-2'
+let element3 = '.js-test-class-3'
+let optionsValidateFailed = {
+	trackerName: 'default_emit', // 预设
+	validateRule: 'test' // 由于有预设，该规则会被覆盖
+}
+let optionsValidateFailed2 = {
+	trackerName: 'diy', // 预设
+	validateRule: 'test' // 由于有预设，该规则会被覆盖
+}
 
-// auto collect example 2
-ATM.autoCollectTrackData({
-	test: 1
-},{
-	trigger: 'click',
-	page: '*',
-	element: '.js-test-class'
-})
+// basic demo
+ATM.autoCollectTrackData('click','.*','.js-test-class-1')
+
+ATM.autoCollectTrackData(trigger,page,element2,optionsValidateFailed)
+
+ATM.autoCollectTrackData(trigger,page,element3,optionsValidateFailed2)
+
+
 
 // auto collect target data  TBD
 // 当页面加载时，获取某hidden input的值或者某全局变量的值 
--- src/ATM.js
+++ src/ATM.js
@@ -2,7 +2,7 @@
 //
 // Auto Track Module
 // 
-// v0.0.2
+// v0.0.4
 // URL: [url=https://github.com/xunge0613/ATM/]https://github.com/xunge0613/ATM[/url]
 //
 // Licensed under the MIT license:
@@ -10,9 +10,8 @@
 //
 //---------------------------------------------------------------------
 ;(function () {
-	const ATM = {}
- 
-	// CONFIG
+
+	// 默认配置 CONFIG
 	// -----------------------------------------------
 	const ATM_CONFIG = {
 		/*
@@ -25,26 +24,14 @@
 			'test': {
 				requiredData: ['test'], 
             	requiredOptions: []
-			},
-			'baidu_auto': {
-				requiredData: [], 
-            	requiredOptions: ['trigger','page', 'element']
-			},
-			'google_auto': {
-				requiredData: [], 
-            	requiredOptions: ['trigger','page', 'element']
-			},
-            'piwik_auto': {
+			},		
+			'default_auto': {
             	requiredData: [], 
-            	requiredOptions: ['trigger','page', 'element']
-            },
-            'default_auto': {
-            	requiredData: [], 
-            	requiredOptions: ['trigger','page', 'element']
+            	requiredOptions: []
             },
             'default_emit': {
             	requiredData: [], 
-            	requiredOptions: []
+            	requiredOptions: ['trigger']
             },
         },
         /*
@@ -72,24 +59,9 @@
 					mergeOptionValue: 1
 				},								
 			],
-			'default_auto': [
-				{
-					mergeDataName: 'category',
-					mergeOptionName: 'page'
-				},
-				{
-					mergeDataName: 'action',
-					mergeOptionName: 'trigger'
-				},
-				{
-					mergeDataName: 'name',
-					mergeOptionName: 'element'
-				},
-				{
-					mergeDataName: 'value',
-					mergeOptionValue: 1
-				},								
-			],
+			'default_auto': function(data,options) {
+				return data
+			},
 			'default_emit': [],
 		},
         /*
@@ -108,10 +80,10 @@
                         try {
                             let atmTracker = Piwik.getTracker();
                             if (data.value) {
-                                atmTracker.trackEvent(data.category, data.action, data.name, data.value);
+                                atmTracker.trackEvent(data.page, data.trigger, data.element, data.value);
                             }
                             else {
-                                atmTracker.trackEvent(data.category, data.action, data.name);
+                                atmTracker.trackEvent(data.page, data.trigger, data.element);
                             }
                         }
                         catch (error) {
@@ -128,6 +100,173 @@
 	 
 	}
 
+	const ATM = {
+		presetRules: {
+			'default_auto': {
+				validateRule: ATM_CONFIG.VALIDATE_RULES.default_auto,
+				processRule: ATM_CONFIG.PROCESS_RULES.default_auto,
+				reportRule: ATM_CONFIG.REPORT_RULES.default_auto,
+			},
+			'default_emit': {
+				validateRule: ATM_CONFIG.VALIDATE_RULES.default_emit,
+				processRule: ATM_CONFIG.PROCESS_RULES.default_emit,
+				reportRule: ATM_CONFIG.REPORT_RULES.default_emit,
+			},
+			'piwik_auto': {
+				validateRule: ATM_CONFIG.VALIDATE_RULES.default_auto,
+				processRule: ATM_CONFIG.PROCESS_RULES.default_auto,
+				reportRule: ATM_CONFIG.REPORT_RULES.default_auto,
+			},
+			'google_auto': {
+				validateRule: ATM_CONFIG.VALIDATE_RULES.default_auto,
+				processRule: ATM_CONFIG.PROCESS_RULES.default_auto,
+				reportRule: ATM_CONFIG.REPORT_RULES.google_auto, // google here
+			}
+		},
+		atmTracker: {
+			// 缓存已创建的 ATMTracker 实例
+			// trackerName: new ATMTracker()
+		}
+	}
+ 
+	// ATMTracker
+	class ATMTracker {
+		constructor(trackerName, data, options) {
+			
+			if(ATM.presetRules[trackerName]) {
+				// 若有预设规则，则使用该规则
+				this.validateRule = ATM.presetRules[trackerName].validateRule
+				this.processRule = ATM.presetRules[trackerName].processRule
+				this.reportRule = ATM.presetRules[trackerName].reportRule
+			} else {
+
+				// 若 ruleName 是 string 则匹配是否存在
+
+
+				// 若 ruleName 是 function 
+
+				// 命名空间
+				// 若预设中不存在，则匹配 options[trackerName + 'options'] 字段是否存在 （用于区分同时多个统计工具）
+				// 若存在，则使用该对象的属性
+				if(options[trackerName + 'options']) {
+					// 分别判断 function or string 
+					(options[trackerName + 'options'].validateRule) && (this.validateRule = options.validateRule ) 
+					(options[trackerName + 'options'].processRule) && (this.processRule = options.processRule )
+					(options[trackerName + 'options'].reportRule) && (this.reportRule = options.reportRule	)	 													
+				} else {
+					// 若都不满足，则取 options.xxxRule
+					this.validateRule = options.validateRule
+					this.processRule = options.processRule
+					this.reportRule = options.reportRule
+				}		
+
+
+			}	
+
+			
+		}
+
+		 
+		validateData(data,options) {
+			console.log('ATM tracker : validateData')
+			
+			if(!this.validateRule) {
+				// 指定规则，若未找到rule, 认定校验失败		 
+				console.warn("ATM validateData: no matched validate rule, Please check your options.validateRule")
+				return false
+			}
+
+			if(typeof this.validateRule === 'function') {
+				// 规则为函数，直接用该函数进行判断
+				return this.validateRule(data,options)
+
+			} else if (typeof this.validateRule === 'object') {
+				// 规则为对象，使用约定方式进行判断			
+				this.validateRule.requiredData = this.validateRule.requiredData || []
+				this.validateRule.requiredOptions = this.validateRule.requiredOptions || []
+				// 任何一个未匹配成功即报错 
+				return !this.validateRule.requiredData.some(function(val) {		 
+					!data[val] && console.warn("ATM validateData: Validate NOT passed, param: data." + val)						
+					return !data[val] 
+				}) && !this.validateRule.requiredOptions.some(function(val) {				
+					!options[val] && console.warn("ATM validateData: Validate NOT passed, param: options." + val)
+					return !options[val] 
+				})  
+				
+			}
+
+
+			console.warn("ATM validateData: Rule unmatched Please check your options.validateRule ")
+			return false
+			
+		}
+
+		processData(data,options) {
+			console.log('ATM tracker : processData')
+
+			if(!this.processRule) {
+			// 指定规则，若未找到rule, 认定处理失败		 
+				console.warn("ATM validateData: no matched process rule, Please check your options.processRule")
+				return false
+			}	
+
+			if(typeof this.processRule === 'function') {
+				// 规则为函数，直接用该函数进行数据处理
+				return this.processRule(data,options)
+
+			} else if (Object.prototype.toString.call(this.processRule) === '[object Array]') {
+				// 规则为数组，使用约定方式进行数据处理								 	
+				// mergeOptionName 优先级高于 mergeOptionValue
+				this.processRule.forEach(function(rule) {				
+					if(typeof rule.mergeOptionValue !== 'undefined') {
+						data[rule.mergeDataName] = options[rule.mergeOptionValue]
+					}
+					if(typeof rule.mergeOptionName !== 'undefined') {
+						data[rule.mergeDataName] = options[rule.mergeOptionName]
+					}
+				})
+				return data 
+			}	
+
+			console.warn("ATM validateData : Rule unmatched Please check your options.processRule ")
+			return false
+		}
+
+		reportData(data,options) {
+			console.log('ATM tracker : reportData')
+
+			if(!this.reportRule) {
+				// 指定规则，若未找到rule, 认定上报失败		 	 
+				console.warn("ATM reportData: no matched report rule, Please check your options.reportRule")
+				return false
+			}
+			if(typeof this.reportRule === 'function') {
+				// 规则为函数，直接用该函数进行判断
+				return this.reportRule(data,options)
+
+			}
+			console.warn("ATM validateData: Rule unmatched Please check your options.reportRule")
+			return false
+		}		
+
+		// emit collecting track data 主动上报
+		emit(data,options) {
+			// 验证
+	    	if(!this.validateData(data, options)) {
+	        	return false
+	        }
+	        // 处理
+	    	let _processedData = this.processData(data, options)	      
+	    	// 上报
+	        if(_processedData) {
+	        	this.reportData(_processedData, options)  
+	        }
+		}
+	} 
+
+
+	
+
 	// RULES
 
  
@@ -135,77 +274,76 @@
 	// -----------------------------------------------
 
 	// 数据校验
-	function validateData(rule, data, options) {
-		let _validateStatus = true 
+	// function validateData(rule, data, options) {
+		
+	// 	if(!ATM_CONFIG.VALIDATE_RULES[rule]) {
+	// 		// 指定规则，若未找到rule, 认定校验失败		 
+	// 		console.warn("ATM validateData: no matched validate rule, Please check your VALIDATE_RULES")
+	// 		return false
+	// 	}
 
-		if(!ATM_CONFIG.VALIDATE_RULES[rule]) {
-			// 指定规则，若未找到rule, 认定校验失败		 
-			console.warn("ATM validateData: no matched validate rule, Please check your VALIDATE_RULES")
-			return false
-		}
+	// 	if(typeof ATM_CONFIG.VALIDATE_RULES[rule] === 'function') {
+	// 		// 规则为函数，直接用该函数进行判断
+	// 		return ATM_CONFIG.VALIDATE_RULES[rule](data,options)
 
-		if(typeof ATM_CONFIG.VALIDATE_RULES[rule] === 'function') {
-			// 规则为函数，直接用该函数进行判断
-			return ATM_CONFIG.VALIDATE_RULES[rule](data,options)
-
-		} else if (typeof ATM_CONFIG.VALIDATE_RULES[rule] === 'object') {
-			// 规则为对象，使用约定方式进行判断			
-			ATM_CONFIG.VALIDATE_RULES[rule].requiredData = ATM_CONFIG.VALIDATE_RULES[rule].requiredData || []
-			ATM_CONFIG.VALIDATE_RULES[rule].requiredOptions = ATM_CONFIG.VALIDATE_RULES[rule].requiredOptions || []
-			// 任何一个未匹配成功即报错 
-			return !ATM_CONFIG.VALIDATE_RULES[rule].requiredData.some(function(val) {		 
-				!data[val] && console.warn("ATM validateData: Validate NOT passed, param: data." + val)						
-				return !data[val] 
-			}) && !ATM_CONFIG.VALIDATE_RULES[rule].requiredOptions.some(function(val) {				
-				!options[val] && console.warn("ATM validateData: Validate NOT passed, param: options." + val)
-				return !options[val] 
-			})  
+	// 	} else if (typeof ATM_CONFIG.VALIDATE_RULES[rule] === 'object') {
+	// 		// 规则为对象，使用约定方式进行判断			
+	// 		ATM_CONFIG.VALIDATE_RULES[rule].requiredData = ATM_CONFIG.VALIDATE_RULES[rule].requiredData || []
+	// 		ATM_CONFIG.VALIDATE_RULES[rule].requiredOptions = ATM_CONFIG.VALIDATE_RULES[rule].requiredOptions || []
+	// 		// 任何一个未匹配成功即报错 
+	// 		return !ATM_CONFIG.VALIDATE_RULES[rule].requiredData.some(function(val) {		 
+	// 			!data[val] && console.warn("ATM validateData: Validate NOT passed, param: data." + val)						
+	// 			return !data[val] 
+	// 		}) && !ATM_CONFIG.VALIDATE_RULES[rule].requiredOptions.some(function(val) {				
+	// 			!options[val] && console.warn("ATM validateData: Validate NOT passed, param: options." + val)
+	// 			return !options[val] 
+	// 		})  
 			
-		}
+	// 	}
 
 
-		console.warn("ATM validateData: Type unmatched Please check your ATM_CONFIG.VALIDATE_RULES ")
-		return false
-	}
+	// 	console.warn("ATM validateData: Type unmatched Please check your ATM_CONFIG.VALIDATE_RULES ")
+	// 	return false
+	// }
 
 	// 数据预处理
-	function processData(rule = '', data = {} , options = {}) { 
-		if(!ATM_CONFIG.PROCESS_RULES[rule]) {
-			// 指定规则，若未找到rule, 认定处理失败		 
-			console.warn("ATM validateData: no matched validate rule, Please check your PROCESS_RULES")
-			return false
-		}	
+	// function processData(rule = '', data = {} , options = {}) { 
+	// 	if(!ATM_CONFIG.PROCESS_RULES[rule]) {
+	// 		// 指定规则，若未找到rule, 认定处理失败		 
+	// 		console.warn("ATM validateData: no matched validate rule, Please check your PROCESS_RULES")
+	// 		return false
+	// 	}	
 
-		if(typeof ATM_CONFIG.PROCESS_RULES[rule] === 'function') {
-			// 规则为函数，直接用该函数进行数据处理
-			return ATM_CONFIG.PROCESS_RULES[rule](data,options)
+	// 	if(typeof ATM_CONFIG.PROCESS_RULES[rule] === 'function') {
+	// 		// 规则为函数，直接用该函数进行数据处理
+	// 		return ATM_CONFIG.PROCESS_RULES[rule](data,options)
 
-		} else if (Object.prototype.toString.call(ATM_CONFIG.PROCESS_RULES[rule]) === '[object Array]') {
-			// 规则为数组，使用约定方式进行数据处理								 	
-			// mergeOptionName 优先级高于 mergeOptionValue
-			ATM_CONFIG.PROCESS_RULES[rule].forEach(function(rule) {				
-				if(typeof rule.mergeOptionValue !== 'undefined') {
-					data[rule.mergeDataName] = options[rule.mergeOptionValue]
-				}
-				if(typeof rule.mergeOptionName !== 'undefined') {
-					data[rule.mergeDataName] = options[rule.mergeOptionName]
-				}
-			})
-			return data 
-		}	
+	// 	} else if (Object.prototype.toString.call(ATM_CONFIG.PROCESS_RULES[rule]) === '[object Array]') {
+	// 		// 规则为数组，使用约定方式进行数据处理								 	
+	// 		// mergeOptionName 优先级高于 mergeOptionValue
+	// 		ATM_CONFIG.PROCESS_RULES[rule].forEach(function(rule) {				
+	// 			if(typeof rule.mergeOptionValue !== 'undefined') {
+	// 				data[rule.mergeDataName] = options[rule.mergeOptionValue]
+	// 			}
+	// 			if(typeof rule.mergeOptionName !== 'undefined') {
+	// 				data[rule.mergeDataName] = options[rule.mergeOptionName]
+	// 			}
+	// 		})
+	// 		return data 
+	// 	}	
 		
-	}
+	// }
 
 	// 数据上报
-	function reportData(rule = '', data = {}, options = {}) {				
-		if(!ATM_CONFIG.REPORT_RULES[rule]) {
-			// 指定规则，若未找到rule, 认定上报失败		 	 
-			console.warn("ATM validateData: no matched validate rule, Please check your REPORT_RULES")
-			return false
-		}
+	// function reportData(rule = '', data = {}, options = {}) {				
+	// 	if(!ATM_CONFIG.REPORT_RULES[rule]) {
+	// 		// 指定规则，若未找到rule, 认定上报失败		 	 
+	// 		console.warn("ATM validateData: no matched validate rule, Please check your REPORT_RULES")
+	// 		return false
+	// 	}
 
-		return ATM_CONFIG.REPORT_RULES[rule](data,options)		 
-	}
+	// 	return ATM_CONFIG.REPORT_RULES[rule](data,options)		 
+	// }
 
 	// API
 	// -----------------------------------------------
@@ -233,37 +371,49 @@
 
         }
     */
-	ATM.autoCollectTrackData = function (data = {}, options = {}) {
+	ATM.autoCollectTrackData = function (trigger, page, element, options = {}) {
 	  
+	  	
 	    // 数据校验，处理，上报都可空，默认使用 default_auto 进行检验
+	    options.trackerName = options.trackerName || 'default_auto'
 	    options.validateRule = options.validateRule || 'default_auto'
 	    options.processRule = options.processRule || 'default_auto'
 	    options.reportRule = options.reportRule || 'default_auto'
 
-	    if(!validateData(options.validateRule, data, options)) {
+	    // 数据预处理
+	    options.desc = options.desc || {}
+	    options.desc = {
+	    	trigger: options.desc.trigger || trigger,
+	    	page: options.desc.page || page,
+	    	element: options.desc.element || element,
+	    	value: options.desc.value || 1
+	    }
+
+	    // 此处只验证埋点信息是否正确，不验证数据是否合法，也不对数据进行处理
+
+
+	    if(!trigger || !page || !element) {
 	    	console.warn("ATM autoCollectTrackData: validateRule NOT passed")
 	    	return false
 	    }
-
-	   	// page === * 表示全部页面都监听
-	    if (options.page === "*") {
-	        options.page = window.location.href.toLowerCase();
-	    }
-
-	    var currenturl = window.location.href.toLowerCase();
+ 
+	   	// page === .* 表示全部页面都监听	    
+	   	// page 默认后期会由后台自动生成，前期需要自己手写正则
+	    let pageRegexp = new RegExp(page,'i')	    
+	    let currenturl = window.location.href.toLowerCase();
 	    try {
-	        if (currenturl.indexOf(options.page) != -1) {
+	        if (currenturl.match(pageRegexp)) {
 	        	if(typeof jQuery === 'undefined') {
 		        	// default : Event Capture 默认捕获方式
-		        	let _nodeList = document.querySelectorAll(options.element)
+		        	let _nodeList = document.querySelectorAll(element)
 
 		        	if(_nodeList) {
 		        		let _elements = Array.prototype.slice.call(_nodeList)
 		        		_elements.forEach(function(_element) {
-		        			_element.addEventListener(options.trigger,function() {			        		 	
+		        			_element.addEventListener(trigger,function() {			        		 	
 			        		 	console.log(this)
-			        		 	console.log('ATM.emitCollectingTrackData(data,options), native, Event Capture')
-			        		 	ATM.emitCollectingTrackData(data,options)
+			        		 	console.log('ATM.emitCollectingTrackData(data,options.desc), native, Event Capture')
+			        		 	ATM.emitCollectingTrackData(options.trackerName, options.desc ,options)
 			        		 })
 		        		})
 		        		 
@@ -271,10 +421,10 @@
 		        	// 如果异步加载，则使用事件冒泡委托代理
 	        	} else {
 	        		// 如果存在jQuery，可以解决兼容性问题
-		            $(options.element).on(options.trigger, data, function () {
+		            $(element).on(trigger, function () {
 		                console.log(this)
 	        		 	console.log('ATM.emitCollectingTrackData(data,options) ,jQuery ,Event Capture')
-	        		 	ATM.emitCollectingTrackData(data,options)
+	        		 	ATM.emitCollectingTrackData(options.trackerName, options.desc ,options)
 		            })
 	        	}	        		        	
 	        }                
@@ -287,77 +437,55 @@
 
 	/*
             主动 触发数据收集
-            @param: data {
-                page: '', // 页面: home/index， 默认是 options.page
-                trigger: '', // 事件名or操作名 ，默认是 options.element + options.trigger
-                element: '', // 触发事件的元素名,默认是 options.element
-                value: Number, // 统计计数，默认是1
-            }
-            @param: options {    
+         	@param: trackerName 统计工具名 
+				type: String/Array
+	        @param: options {    
                 processType: function() {} // 数据处理方式
             }
             data 和 options 不能同时为空，单一可空
             data 为空时，可能是通过 被动方式 autoCollectingTrackData
             options 为空时， 一般是 直接调用主动方式 emitCollectingTrackData
         */
-    ATM.emitCollectingTrackData = function (data, options) {
+    ATM.emitCollectingTrackData = function (trackerName, data, options) {
         if (!data && !options) {
             return false;
         }
-        // options 为空时，认定为通过主动收集调用，填充默认 processRule, validateRule, reportRule
-        options = options || { 
-        	processRule: 'default_emit',
-        	validateRule: 'default_emit',
-        	reportRule: 'default_emit'
-        }
 
-        // 默认使用default_emit校验数据
-        if (!options.validateRule) {
-            options.validateRule = 'default_emit'
-        } 
-
-        // 默认使用default_emit处理数据
-        if (!options.processRule) {
-            options.processRule = 'default_emit'
-        } 
-
-        // 默认使用default_emit上报
-        if (!options.reportRule) {
-            options.reportRule = 'default_emit'
-        }
-
-        console.log('emitCollectingTrackData', data, options)   
-
-        // 验证数据         
-        if(!validateData(options.validateRule, data, options)) {
-        	return false
-        }
-       
         
+        // options 为空时，认定为通过主动收集调用，填充默认 
+        options = options || {         	 
+        	trackerName: 'default_emit'
+        }
+
+        console.log('emitCollectingTrackData', data, options)          
 
         // 上报数据
         // 如果有多个 reportRule
         // 约定使用reportRule作为processRule
 	 
-        if(typeof options.reportRule === 'string') {
-        	// 处理单条数据          
-	        // 若返回 false 表示数据处理失败
-	        data = processData(options.processRule, data, options)
-	        if(data) {
-	        	reportData(options.reportRule, data, options)  
-	        }		 
-		} else if (Object.prototype.toString.call(options.reportRule) === '[object Array]') {
-			options.reportRule.forEach(function(rule) {
-
-				// 处理数据          
-		        // 若返回 false 表示数据处理失败
-		        let _processedData = processData(options.processRule, data, options)		      
-		        if(_processedData) {
-		        	reportData(rule, _processedData, options)  
-		        }
-				
+        if(typeof options.trackerName === 'string') {        	
+	        _applyATMTracker(options.trackerName, data, options)	      	         
+		} else if (Object.prototype.toString.call(options.trackerName) === '[object Array]') {
+			options.trackerName.forEach(function(_trackerName) {				
+		        _applyATMTracker(_trackerName, data, options)
 			})
 		}                      
+    }
+
+    // 全局注册 ATMTracker
+    function _initATMTracker(trackerName, data, options) {   
+    	// 调用 ATM Tracker 进行数据验证、处理、分析
+        // 同时在全局注册该 tracker 实例， 以避免重复new实例	
+		if(!ATM.atmTracker[trackerName]) {
+			ATM.atmTracker[trackerName] = new ATMTracker(trackerName, data, options)
+		}   
+    }
+
+    function _applyATMTracker(trackerName, data, options) {    
+    	// 全局注册 ATMTracker	
+    	_initATMTracker(trackerName, data, options) 
+    	// 调用公共方法 emit 进行上报 -> 内部包含 validate, process, report
+    	ATM.atmTracker[options.trackerName].emit(data,options)
     }
 
 
